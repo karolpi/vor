add6c0d49959c8fdbfd2a69a37632f212ffaa0ec
TajemniczyKrawat <karol.piechurap1@wp.pl>
2019-07-15 10:40:01 +0200

    working intersection accross Perpendiculars

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,5 +9,10 @@ object Main {
     val file: File = new File("image.png")
 
     ImageIO.write(b, "png", file)
+    val line1 = new Perpendicular(new Point(-3,-3), new Point(1,-1))
+    val line2 = new Perpendicular(new Point(0,2), new Point(1,5))
+    println(line1)
+    println(line2)
+    println(line2.findIntersection(line1))
   }
 }
--- src/main/scala/Perpendicular.scala
+++ src/main/scala/Perpendicular.scala
@@ -2,7 +2,18 @@ import scala.math.abs
 
 class Perpendicular(p1: Point, p2: Point) {
   val centerPoint: Point = new Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
-  val a: Double = -1 / (p2.y - p1.y) / (p2.x - p1.x)
-  val b: Double = centerPoint.y - a * centerPoint.x
-  val c: Double =
+  val a: Double = -1 / ((p2.y - p1.y) / (p2.x - p1.x))
+  val b: Double = -1
+  val c: Double = a * centerPoint.x - centerPoint.y
+
+  def findIntersection(line: Perpendicular): Point = {
+    val w = this.a * line.b - line.a * this.b
+    val wx = this.b * line.c - line.b * this.c
+    val wy = this.a * line.c - line.a * this.c
+    new Point(wx / w, wy / w)
+  }
+
+  override def toString: String = {
+    s"centerPoint: ${centerPoint.x},${centerPoint.y} $a * ${centerPoint.x} + $b * ${centerPoint.y} = $c"
+  }
 }
